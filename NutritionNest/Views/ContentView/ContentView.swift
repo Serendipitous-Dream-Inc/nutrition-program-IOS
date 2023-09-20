b42c05dea1ed244aadd2061cba7c86fe0e4ebe97
Pedro Ésli <pedroesli.vn@gmail.com>
2023-09-20 18:12:10 -0300

    feat: moved project folder one folder and added swiftlint

--- NutritionNest/Views/ContentView/ContentView.swift
+++ NutritionNest/Views/ContentView/ContentView.swift
@@ -16,6 +16,9 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            let noWarning = NSNumber() as! Int
+        }
     }
 }
 
