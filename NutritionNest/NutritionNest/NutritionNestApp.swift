d16500c14243aef08f8bf17256513a2a4a46bd38
Pedro Ésli <pedroesli.vn@gmail.com>
2023-09-20 15:35:09 -0300

    feat: created xcode project

---
+++ NutritionNest/NutritionNest/NutritionNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutritionNestApp.swift
+//  NutritionNest
+//
+//  Created by Pedro Ésli Vieira do Nascimento on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct NutritionNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
