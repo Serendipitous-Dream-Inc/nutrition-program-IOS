cad9455ea6694a8822fed8376e5d1f84d4ae8ab6
Pedro Ésli <pedroesli.vn@gmail.com>
2023-09-20 16:29:35 -0300

    feat: created views group

---
+++ NutritionNest/NutritionNest/Views/LogInView/LogInView.swift
@@ -0,0 +1,20 @@
+//
+//  LogInView.swift
+//  NutritionNest
+//
+//  Created by Pedro Ésli Vieira do Nascimento on 20/09/23.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LogInView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogInView()
+    }
+}
