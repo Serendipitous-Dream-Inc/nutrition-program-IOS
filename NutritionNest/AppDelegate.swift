6051aa19a62f9972a3be5c67614f31783db8aa9c
Pedro Ésli <pedroesli.vn@gmail.com>
2023-09-26 21:56:24 -0300

    feat: added firebase dependency

---
+++ NutritionNest/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  NutritionNest
+//
+//  Created by Pedro Ésli Vieira do Nascimento on 26/09/23.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
