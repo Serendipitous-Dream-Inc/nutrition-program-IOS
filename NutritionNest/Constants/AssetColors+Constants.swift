4bae6d25dfdb076f328f7783e8d65ebc4bf64ab3
Pedro Ésli <pedroesli.vn@gmail.com>
2023-10-03 12:22:13 -0300

    feat: created assetcolors

---
+++ NutritionNest/Constants/AssetColors+Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Colors+Constants.swift
+//  NutritionNest
+//
+//  Created by Pedro Ésli Vieira do Nascimento on 03/10/23.
+//
+
+import SwiftUI
+
+struct AssetColors {
+    static let background = Color("BackgroundColor")
+}
+
+extension Color {
+    static let assetColors = AssetColors()
+}
