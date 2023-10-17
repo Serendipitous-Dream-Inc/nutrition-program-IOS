c8e23a302c6b9c1bf2bd7aa60493b0e5383433a7
Oliverl11 <145501997+Oliverl11@users.noreply.github.com>
2023-10-16 21:19:30 -0400

    feat: changed Onboarding View

--- NutritionNest/Views/Onboarding/OnboardingView.swift
+++ NutritionNest/Views/Onboarding/OnboardingView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  Loginpage
 //
-//  Created by LiTianyi on 2023/9/27.
+//  Created by Oliver on 2023/9/27.
 //
 
 import SwiftUI
@@ -10,33 +10,34 @@ import SwiftUI
 struct OnboardingView: View {
     var body: some View {
         VStack {
-            Text(Localization.Onboarding.welcome).font(.system(size: 26)).bold().padding()
-            Image("logo")
+            Text(Localization.Onboarding.welcome).font(.custom("Inter", size: 26)).bold().padding()
+            Image.assets.logos.appLogo
                 .resizable()
-                   .scaledToFit()
-                   .frame(width: 300, height: 460)
-                   .foregroundColor(.accentColor)
-                   .padding(.top, 10)
-            
-            Button(action: {
-                // Button action code here
-            }) {
+                .scaledToFit()
+                .frame(width: 300, height: 460)
+                .foregroundColor(.accentColor)
+                .padding(.top, 10)
+
+            Button {
+
+            } label: {
                 Text(Localization.Onboarding.signup)
-                    .font(.system(size: 18)).bold()
+                    .font(.custom("Inter", size: 18)).bold()
                     .padding(EdgeInsets(top: 28, leading: 26, bottom: 28, trailing: 26))
                     .background(Color.accentColor)
                     .foregroundColor(.white)
                     .cornerRadius(28)
                     .padding(.top, -5)
-                       }
+            }
+
             HStack {
                 Text(Localization.Onboarding.account)
-                    .font(.system(size: 18))
-                Button(action: {
-                    // Add button action code here
-                }) {
+                    .font(.custom("Inter", size: 18))
+                Button {
+
+                } label: {
                     Text(Localization.Onboarding.login)
-                        .font(.system(size: 18))
+                        .font(.custom("Inter", size: 18))
                         .bold()
                         .foregroundColor(.black)
                         .underline()
@@ -54,4 +55,3 @@ struct OnboardingView_Previews: PreviewProvider {
         OnboardingView()
     }
 }
-
