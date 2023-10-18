f685063253dd107263a661e82f16523c377ae51e
Oliverl11 <145501997+Oliverl11@users.noreply.github.com>
2023-10-17 23:37:40 -0400

    feat: changed Onboarding View

--- NutritionNest/Views/Onboarding/OnboardingView.swift
+++ NutritionNest/Views/Onboarding/OnboardingView.swift
@@ -4,13 +4,12 @@
 //
 //  Created by Oliver on 2023/9/27.
 //
-
 import SwiftUI
 
 struct OnboardingView: View {
     var body: some View {
         VStack {
-            Text(Localization.Onboarding.welcome).font(.custom("Inter", size: 26)).bold().padding()
+            Text(Localization.Onboarding.welcome).font(.custom.inter.font(size: 26)).bold().padding()
             Image.assets.logos.appLogo
                 .resizable()
                 .scaledToFit()
@@ -22,7 +21,7 @@ struct OnboardingView: View {
 
             } label: {
                 Text(Localization.Onboarding.signup)
-                    .font(.custom("Inter", size: 18)).bold()
+                    .font(.custom.inter.font(size: 18)).bold()
                     .padding(EdgeInsets(top: 28, leading: 26, bottom: 28, trailing: 26))
                     .background(Color.accentColor)
                     .foregroundColor(.white)
@@ -32,12 +31,12 @@ struct OnboardingView: View {
 
             HStack {
                 Text(Localization.Onboarding.account)
-                    .font(.custom("Inter", size: 18))
+                    .font(.custom.inter.font(size: 18))
                 Button {
 
                 } label: {
                     Text(Localization.Onboarding.login)
-                        .font(.custom("Inter", size: 18))
+                        .font(.custom.inter.font(size: 18))
                         .bold()
                         .foregroundColor(.black)
                         .underline()
